f2badb30f3cc85c7d39f35c763107879b2f038ef
Yuri-ren <renyouyin@gmail.com>
2015-10-16 16:21:29 +0800

    增加多进程或者多线程下载

--- taobao_img.py
+++ taobao_img.py
@@ -4,31 +4,23 @@ import requests
 import json
 import os
 import threading
+#import mutilprocessing
+from multiprocessing import Process
 
 
 ########淘宝页面商品详情为js动态加载，所以python抓取静态页面的方法无法获取到图片地址，采用直接抓取js地址，然后对其进行请求，得到json字符串，得到图片文件名
 __author__='Yuri'
 
-####淘宝图片地址，后续进行图片地址拼接
-img_server='http://img.alicdn.com/imgextra/'
-
-
 ###re pattern
 title_pattern=re.compile(r'<title>.*</title>') 
 ###修改寻找js的正则表达式,有两个，desc.alicdn.com和dsc.taobaocdn.com,任取其一都可以.注意，此处服务器与下文请求js得出的img地址相关
-#js_pattern=re.compile(r'apiImgInfo:"//otds.alicdn.com/json/item_imgs.htm.*?"')
 js_pattern=re.compile(r'desc.alicdn.com.[^"]*')
 ###由js请求结果过滤img地址的pattern
 img_js_pattern=re.compile(r'img.alicdn.com.[^"]*')
-js2dict_pattern=re.compile(r'{.*}',re.S)
-filetype_pattern=re.compile(r'.*\..*')
 
 #####将unicode字符转换为utf编码
 def u2utf8(temp_string):
 	return temp_string.encode('utf8')
-	
-	
-	
 
 ####get html page
 ####根据系统,windows|linux来显示提示字符,防止乱码
@@ -61,20 +53,19 @@ if (platform=='nt'):
 
 img_dir=page_title+'/'
 
-
 ##线程函数
 def proc(temp_url,thread_count):
 	img_name=str(temp_url).split('/')[-1]
 	img_path=img_dir+img_name
 	#print u"正在下载第",img_count,u"张图片"
 	print u"开始下载图片---->",img_name
-	print u"线程编号为:",thread_count
+	print u"线程或者进程编号为:",thread_count
 	img_req=requests.get(temp_url)
 	with open(img_path,'wb') as temp_file:
 		for temp_chunk in img_req.iter_content(chunk_size=1024):
 			temp_file.write(temp_chunk)
 	print img_name,u"下载完成~~~~"
-	print u"编号为",thread_count,u"线程退出~~~"
+	print u"编号为",thread_count,u"的线程或者进程退出~~~"
 
 ####得到生成图片的js地址
 js_temp=js_pattern.findall(html_page)[0]
@@ -94,18 +85,19 @@ def img_url_map(temp_url):
 	return img_url
 ##最终得到的img地址列表
 img_url=map(img_url_map,img_url_list)
-#print img_url
 ##图片总数
 img_num=len(img_url)
 
-####下载图片文件
 #img_count=1
-###图片存放目录，均为当前标题目录
-#print img_dir
 print u"当前页面商品图片总数为",img_num
 
 thread_count=0
 for i in img_url:
 	thread_count+=1
-	t=threading.Thread(target=proc,args=(i,thread_count))
-	t.start()
+	##mutil threads OR mutil processess
+	###mutil threads
+	#t=threading.Thread(target=proc,args=(i,thread_count))
+	#t.start()
+	###mutil processes
+	p=Process(target=proc,args=(i,thread_count))
+	p.start()
