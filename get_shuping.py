0190129be3fdb78028710c5dc70eccfde4a16a99
renyouyin <renyouyin@xunlei.com>
2017-07-31 20:13:13 +0800

    test

---
+++ get_shuping.py
@@ -0,0 +1,5 @@
+#coding: utf-8
+#coding: utf-8
+
+
+url='http://api.data.p2cdn.com/v2/topic/domain/pull/out/pro/isp/day/9e3e9c45c217015d1c032f909e5e4aa2?domain=pl8.live.panda.tv&country!=230&sdate=2017-07-29~2017-07-29'
