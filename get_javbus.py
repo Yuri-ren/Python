037761598b5c88a21448af3de2924cc2a43b2b1f
Yuri@pc <renyouyin@gmail.com>
2019-11-23 23:32:24 +0800

    Create get_javbus.py
    add

---
+++ get_javbus.py
@@ -0,0 +1,13 @@
+#coding: utf-8
+import requests
+from bs4 import BeautifulSoup
+
+##当前girl全量页面
+def get_fh_section(url):
+    soup=BeautifulSoup(requests.get(url).text,"html.parser")
+    print(soup)
+
+##test
+if __name__=="__main__":
+    get_fh_section('https://www.javbus.com/star/2eg')
+    print("你好")
